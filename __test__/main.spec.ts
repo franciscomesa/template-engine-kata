f85e30c373941ba1fbf85eb4f31a3aa68a0d3d7e
Francisco Mesa <francisco@leanmind.es>
2023-04-14 12:57:39 +0100

    ✅ Add edge tests cases

--- __test__/main.spec.ts
+++ __test__/main.spec.ts
@@ -40,13 +40,13 @@ describe('Template library should', () => {
   })
 
   describe ('Edge cases and exceptions', () =>  {
-    // TODO: Empty string is an edge case?
     it.skip('throw exception if dictionary is null', () => {
-
+      // Record type cannot be null at typescript.
+      // Maybe using other structure like Map.
     })
 
     it.skip('throw exception if text is null', () => {
-
+      // string primitive type cannot be null at typescript.
     })
 
     it('do not replace empty string', () => {
@@ -65,16 +65,24 @@ describe('Template library should', () => {
       expect(result).toBe('Menganito ${apellido} de tal')
     })
 
-    it.skip('variables not found in the dictionary', () => {
+    it('variables not found in the dictionary', () => {
+      const dictionary: Record <string, string> = { pronombreFemenino: 'Ella' }
 
+      const result = TemplateLibrary.replace('${pronombreFemenino} menganita de tal. ${pronombreMasculino} menganito de tal', dictionary)
+
+      expect(result).toBe('Ella menganita de tal. ${pronombreMasculino} menganito de tal')
     })
 
-    it.skip('variables not used in the dictionary', () => {
+    it('variables not used in the dictionary', () => {
+      const dictionary: Record <string, string> = { pronombreFemenino: 'Ella', pronombreMasculino: 'El' }
 
+      const result = TemplateLibrary.replace('Ella menganita de tal. ${pronombreMasculino} menganito de tal', dictionary)
+
+      expect(result).toBe('Ella menganita de tal. El menganito de tal')
     })
 
     it.skip('empty string as a key in the dictionary', () => {
-
+        // Record type cannot have empty string as a key at typescript.
     })
     // and the case of values that are not convertible to string (not serializable)...
     // How would you design the library in a way that is resilient, intuitive, and comfortable to use?
