496b28ad7cac0ead15b7454636f9808bd8640470
Francisco Mesa <francisco@leanmind.es>
2023-04-14 12:05:33 +0100

    ✨ empty string

--- __test__/main.spec.ts
+++ __test__/main.spec.ts
@@ -1,8 +1,18 @@
-import { Main } from '../src/main';
+class TemplateLibrary {
+  // @ts-ignore
+  replace(s: string, dictionary: Record <string, string>): string {
+    return s
+  }
 
-describe('Default test', () => {
-  it('should work', () => {
-    const main = new Main('Hello world');
-    expect(main.getParam()).toBe('Hello world');
+}
+
+describe('Template library should', () => {
+  it('Do not replace empty string', () => {
+    const template = new TemplateLibrary();
+    const dictionary: Record <string, string> = { };
+
+    const result = template.replace('', dictionary);
+
+    expect(result).toBe('');
   });
 });
