753835bce8811824c24c6d7a4bfb1b0b5a00d3fd
Francisco Mesa <francisco@leanmind.es>
2023-04-14 12:45:18 +0100

    ✅ Add new tests cases

--- __test__/main.spec.ts
+++ __test__/main.spec.ts
@@ -13,28 +13,12 @@ class TemplateLibrary {
 describe('Template library should', () => {
   new TemplateLibrary();
   describe('do not replace', () => {
-    it('empty string', () => {
-      const dictionary: Record <string, string> = { }
-
-      const result = TemplateLibrary.replace('', dictionary)
-
-      expect(result).toBe('');
-    })
-
     it('text without variables', () => {
       const dictionary: Record <string, string> = { }
 
       const result = TemplateLibrary.replace('Menganito de tal', dictionary);
 
       expect(result).toBe('Menganito de tal')
-    })
-
-    it('text if dictionary does not have variables', () => {
-      const dictionary: Record <string, string> = { }
-
-      const result = TemplateLibrary.replace('Menganito ${apellido} de tal', dictionary)
-
-      expect(result).toBe('Menganito ${apellido} de tal')
     })
   })
 
@@ -57,6 +41,43 @@ describe('Template library should', () => {
 
   describe ('Edge cases and exceptions', () =>  {
     // TODO: Empty string is an edge case?
+    it.skip('throw exception if dictionary is null', () => {
+
+    })
+
+    it.skip('throw exception if text is null', () => {
+
+    })
+
+    it('do not replace empty string', () => {
+      const dictionary: Record <string, string> = { }
+
+      const result = TemplateLibrary.replace('', dictionary)
+
+      expect(result).toBe('');
+    })
+
+    it('do not replace text if dictionary does not have variables', () => {
+      const dictionary: Record <string, string> = { }
+
+      const result = TemplateLibrary.replace('Menganito ${apellido} de tal', dictionary)
+
+      expect(result).toBe('Menganito ${apellido} de tal')
+    })
+
+    it.skip('variables not found in the dictionary', () => {
+
+    })
+
+    it.skip('variables not used in the dictionary', () => {
+
+    })
+
+    it.skip('empty string as a key in the dictionary', () => {
+
+    })
+    // and the case of values that are not convertible to string (not serializable)...
+    // How would you design the library in a way that is resilient, intuitive, and comfortable to use?
   })
 
 })
